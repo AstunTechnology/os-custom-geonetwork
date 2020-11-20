a8c08f0ae914678811bf2b60eaa71defc0440db7
archaeogeek <jo.k.cook@gmail.com>
2020-11-20 12:25:06 +0000

    added postgresql config into docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,6 +3,54 @@ set -e
 
 if [ "$1" = 'catalina.sh' ]; then
 
+  if [ ! -d "$DATA_DIR" ]; then
+        echo "$Data directory '$DATA_DIR' does not exist. Creating it..."
+        mkdir -p "$DATA_DIR"
+    fi
+
+    #Set geonetwork data dir
+    export CATALINA_OPTS="$CATALINA_OPTS -Dgeonetwork.dir=$DATA_DIR"
+
+    #Setting host (use $DB_HOST if it's set, otherwise use "postgres")
+    DB_HOST="${DB_HOST:-postgres}"
+    echo "db host: $DB_HOST"
+
+    #Setting port
+    DB_PORT="${POSTGRES_DB_PORT:-5432}"
+    echo "db port: $DB_PORT"
+
+    if [ -z "$DB_USERNAME" ] || [ -z "$DB_PASSWORD" ]; then
+        echo >&2 "you must set DB_USERNAME and DB_PASSWORD"
+        exit 1
+    fi
+
+
+    #Create databases, if they do not exist yet (http://stackoverflow.com/a/36591842/433558)
+    echo  "$DB_HOST:$DB_PORT:*:$DB_USERNAME:$DB_PASSWORD" > ~/.pgpass
+    chmod 0600 ~/.pgpass
+    if psql -h "$DB_HOST" -U "$DB_USERNAME" -p "$DB_PORT" -tqc "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'" | grep -q 1; then
+        echo "Database '$DB_NAME' exists; skipping createdb"
+    elif psql -h "$DB_HOST" -U "$DB_USERNAME" -p "$DB_PORT" -d "$DB_NAME" -tqc "SELECT 1 FROM pg_database WHERE datname = '$DB_NAME'" | grep -q 1;then
+        echo "Database '$DB_NAME' already exist; skipping database creation"
+    else
+        echo "Database '$DB_NAME' doesn't exist. Creating it..."
+        createdb -h "$DB_HOST" -U "$DB_USERNAME" -p "$DB_PORT" -O "$DB_USERNAME" "$DB_NAME"
+    fi
+    rm ~/.pgpass
+
+    #Write connection string for GN
+    sed -ri '/^jdbc[.](username|password|database|host|port)=/d' /usr/local/tomcat/webapps/geonetwork/WEB-INF/config-db/jdbc.properties
+    echo "jdbc.username=$DB_USERNAME" >> /usr/local/tomcat/webapps/geonetwork/WEB-INF/config-db/jdbc.properties
+    echo "jdbc.password=$DB_PASSWORD" >> /usr/local/tomcat/webapps/geonetwork/WEB-INF/config-db/jdbc.properties
+    echo "jdbc.database=$DB_NAME" >> /usr/local/tomcatwebapps/geonetwork/WEB-INF/config-db/jdbc.properties
+    echo "jdbc.host=$DB_HOST" >> /usr/local/tomcat/webapps/geonetwork/WEB-INF/config-db/jdbc.properties
+    echo "jdbc.port=$DB_PORT" >> /usr/local/tomcat/webapps/geonetwork/WEB-INF/config-db/jdbc.properties
+
+    #Fixing an hardcoded port on the connection string (bug fixed on development branch)
+    sed -i -e 's#5432#${jdbc.port}#g' /usr/local/tomcat/webapps/geonetwork/WEB-INF/config-db/postgres.xml
+
+
+
 	mkdir -p "$DATA_DIR"
 
 	#Set geonetwork data dir

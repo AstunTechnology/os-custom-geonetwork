e913eb97f0f70410727fb565661602cb8203c0c5
archaeogeek <jo.k.cook@gmail.com>
2021-01-13 16:59:07 +0000

    added images folder and thesauri and made tweaks to ecs and container config

---
+++ docker/schema_prep-os.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+yum -y install unzip
+cd /home/ec2-user && \
+curl -fSL https://github.com/AstunTechnology/iso19139.gemini23/archive/3.10.x.zip -o gemini23.zip && \
+mkdir schemas && \
+unzip gemini23.zip -d schemas && \
+curl -fSL https://github.com/AstunTechnology/iso19139.gemini22_GN3/archive/3.8.x.zip -o gemini22.zip && \
+unzip gemini22.zip -d schemas && \
+#curl -fSL https://raw.githubusercontent.com/geonetwork/docker-geonetwork/master/3.10.5/postgres/jdbc.properties -o jdbc.properties && \
+mkdir pgdata && \
+mkdir esdata && \
+mkdir geonetwork && \

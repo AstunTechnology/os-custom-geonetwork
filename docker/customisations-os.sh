4a7caf3b5f9c7170a6065746d658fcd614ae2b00
archaeogeek <jo.k.cook@gmail.com>
2021-01-14 17:04:01 +0000

    allow unzip to overwrite files and chown to work recursively, and fix path to os-csw-api.inc

--- docker/customisations-os.sh
+++ docker/customisations-os.sh
@@ -2,14 +2,14 @@
 cd /home/ec2-user && \
 curl -fSL https://github.com/AstunTechnology/iso19139.gemini23/archive/3.10.x.zip -o gemini23.zip && \
 mkdir -p schemas && \
-unzip gemini23.zip -d schemas && \
+unzip -o gemini23.zip -d schemas && \
 curl -fSL https://github.com/AstunTechnology/iso19139.gemini22_GN3/archive/3.8.x.zip -o gemini22.zip && \
-unzip gemini22.zip -d schemas && \
+unzip -o gemini22.zip -d schemas && \
 #curl -fSL https://raw.githubusercontent.com/geonetwork/docker-geonetwork/master/3.10.5/postgres/jdbc.properties -o jdbc.properties && \
 mkdir -p pgdata && \
 mkdir -p esdata && \
 mkdir -p geonetwork/thesauri && \
 mkdir -p nginx && \
-chown ec2-user:ec2-user pgdata esdata geonetwork nginx && \
-cp /home/ec2-user/os-custom-geonetwork/nginx/* /home/ec2-user/nginx && \
-cp -r /home/ec2-user/os-custom-geonetwork/thesauri/*.rdf /home/ec2-user/geonetwork/thesauri
+chown -R ec2-user:ec2-user pgdata esdata geonetwork nginx && \
+cp -f /home/ec2-user/os-custom-geonetwork/nginx/* /home/ec2-user/nginx && \
+cp -rf /home/ec2-user/os-custom-geonetwork/thesauri/*.rdf /home/ec2-user/geonetwork/thesauri

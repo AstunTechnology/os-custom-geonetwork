324dae84edbc8e691184985ccd020125c17a1a0c
archaeogeek <jo.k.cook@gmail.com>
2021-01-14 10:13:23 +0000

    modified server prep script so it calls the customisations script to make it easier to deploy changes without needing to bring the cluster down

---
+++ docker/server_prep-os.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+yum -y install unzip git
+cd /home/ec2-user && \
+git clone https://github.com/AstunTechnology/os-custom-geonetwork.git && \
+chmod +x os-custom-geonetwork/docker/os_prep.sh && \
+./os-custom-geonetwork/docker/customisations-os.sh
+# curl -fSL https://github.com/AstunTechnology/iso19139.gemini23/archive/3.10.x.zip -o gemini23.zip && \
+# mkdir schemas && \
+# unzip gemini23.zip -d schemas && \
+# curl -fSL https://github.com/AstunTechnology/iso19139.gemini22_GN3/archive/3.8.x.zip -o gemini22.zip && \
+# unzip gemini22.zip -d schemas && \
+# #curl -fSL https://raw.githubusercontent.com/geonetwork/docker-geonetwork/master/3.10.5/postgres/jdbc.properties -o jdbc.properties && \
+# mkdir pgdata && \
+# mkdir esdata && \
+# mkdir geonetwork && \
+# mkdir nginx

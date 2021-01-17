cf4d18fb87aec4688ec696c7e68c756167c208d8
archaeogeek <jo.k.cook@gmail.com>
2021-01-17 18:53:27 +0000

    a draft shell script for prepping elasticsearch and kibana

--- docker/customisations-os.sh
+++ docker/customisations-os.sh
@@ -5,14 +5,9 @@ mkdir -p schemas && \
 unzip -o gemini23.zip -d schemas && \
 curl -fSL https://github.com/AstunTechnology/iso19139.gemini22_GN3/archive/3.8.x.zip -o gemini22.zip && \
 unzip -o gemini22.zip -d schemas && \
-mkdir -p pgdata && \
-mkdir -p esdata && \
-mkdir -p geonetwork/thesauri && \
-mkdir -p nginx && \
-mkdir -p kibana && \
-mkdir -p elasticsearch && \
-chown -Rf ec2-user:ec2-user pgdata esdata geonetwork nginx kibana elasticsearch && \
+mkdir -p pgdata esdata geonetwork/thesauri nginx kibana elasticsearch && \
 cp -rf /home/ec2-user/os-custom-geonetwork/nginx/* /home/ec2-user/nginx && \
 cp -rf /home/ec2-user/os-custom-geonetwork/kibana/* /home/ec2-user/kibana  && \
 cp -rf /home/ec2-user/os-custom-geonetwork/elasticsearch/* /home/ec2-user/elasticsearch  && \
-cp -rf /home/ec2-user/os-custom-geonetwork/thesauri/*.rdf /home/ec2-user/geonetwork/thesauri
+cp -rf /home/ec2-user/os-custom-geonetwork/thesauri/* /home/ec2-user/geonetwork/thesauri && \
+chown -Rf ec2-user:ec2-user pgdata esdata geonetwork nginx kibana elasticsearch

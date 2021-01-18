9f0657fa861e0eecd4cd447c367c783f85756b38
archaeogeek <jo.k.cook@gmail.com>
2021-01-18 18:20:12 +0000

    adjustments to copy various conversion files to the correct places

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # os-custom-geonetwork
 Customisations for Ordnance Survey GeoNetwork implementation
+
+See https://github.com/AstunTechnology/os-custom-geonetwork/wiki for developer documentation
--- docker/customisations-os.sh
+++ docker/customisations-os.sh
@@ -8,6 +8,7 @@ unzip -o gemini22.zip -d schemas && \
 mkdir -p pgdata esdata geonetwork/thesauri nginx kibana elasticsearch && \
 cp -rf /home/ec2-user/os-custom-geonetwork/nginx/* /home/ec2-user/nginx && \
 cp -rf /home/ec2-user/os-custom-geonetwork/kibana/* /home/ec2-user/kibana  && \
+cp -rf /home/ec2-user/os-custom-geonetwork/geonetwork/* /home/ec2-user/geonetwork  && \
 cp -rf /home/ec2-user/os-custom-geonetwork/elasticsearch/* /home/ec2-user/elasticsearch  && \
 cp -rf /home/ec2-user/os-custom-geonetwork/thesauri/* /home/ec2-user/geonetwork/thesauri && \
 chown -Rf ec2-user:ec2-user pgdata esdata geonetwork nginx kibana elasticsearch

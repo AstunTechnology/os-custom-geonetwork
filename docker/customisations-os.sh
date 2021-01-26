ac62e29d709dfa3952a0efc0161c1177c45dd4f5
archaeogeek <jo.k.cook@gmail.com>
2021-01-26 17:47:00 +0000

    going back to having everything in the ecs docker-compose file because ecs-cli doesn't accept multiple compose files like docker-compose does

--- docker/customisations-os.sh
+++ docker/customisations-os.sh
@@ -7,12 +7,15 @@ unzip -o gemini23.zip -d schemas && \
 curl -fSL https://github.com/AstunTechnology/iso19139.gemini22_GN3/archive/3.8.x.zip -o gemini22.zip && \
 unzip -o gemini22.zip -d schemas && \
 # directories
-mkdir -p pgdata esdata geonetwork/thesauri nginx kibana elasticsearch && \
+mkdir -p pgdata esdata geonetwork/thesauri nginx kibana elasticsearch postgresql && \
 cp -rf /home/ec2-user/os-custom-geonetwork/nginx/* /home/ec2-user/nginx && \
 cp -rf /home/ec2-user/os-custom-geonetwork/kibana/* /home/ec2-user/kibana  && \
 cp -rf /home/ec2-user/os-custom-geonetwork/geonetwork/* /home/ec2-user/geonetwork  && \
 cp -rf /home/ec2-user/os-custom-geonetwork/elasticsearch/* /home/ec2-user/elasticsearch  && \
 cp -rf /home/ec2-user/os-custom-geonetwork/thesauri/* /home/ec2-user/geonetwork/thesauri && \
-chown -Rf ec2-user:ec2-user pgdata esdata geonetwork nginx kibana elasticsearch && \
+cp -rf /home/ec2-user/postgresql/* /home/ec2-user/postgresql && \
 # postgresql audit script (in progress)
-git clone https://github.com/AstunTechnology/audit-trigger.git
+git clone https://github.com/AstunTechnology/audit-trigger.git && \
+cp -rf audit-trigger/audit.sql /home/ec2-user/postgresql && \
+# finally make sure we can access all the directories
+chown -Rf ec2-user:ec2-user pgdata esdata geonetwork nginx kibana elasticsearch postgresql

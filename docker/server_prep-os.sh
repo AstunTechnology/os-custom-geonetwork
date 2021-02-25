3652d940d1f021b4becbb9fbe81ac5de00c64d0d
archaeogeek <jo.k.cook@gmail.com>
2021-02-25 19:38:06 +0000

    more security tweaks from docker-bench-security

--- docker/server_prep-os.sh
+++ docker/server_prep-os.sh
@@ -17,15 +17,15 @@ echo "-w /etc/default/docker -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
 echo "-w /etc/docker/daemon.json -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
 echo "-w /usr/bin/docker-containerd -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
 echo "-w /usr/bin/docker-runc -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
+echo "-w docker.socket -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
+echo "-w docker.service -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
 sudo service auditd restart
 
-# docker-bench-security issues 2.1-2.15
+# docker-bench-security issues 2.1-2.15 and 4.5
+echo "DOCKER_CONTENT_TRUST=1" | sudo tee -a /etc/environment
 sudo mv ./os-custom-geonetwork/docker/docker-security/daemon.json /etc/docker/daemon.json
 sudo chown root:root /etc/docker/daemon.json
 sudo service docker restart
 
-#add to ecs-config
-# sudo echo "ECS_CLUSTER=docker-geonetwork-ec2" | sudo tee -a /etc/ecs/ecs.config
-# sudo echo "ECS_ENABLE_AWSLOGS_EXECUTIONROLE_OVERRIDE=true" | sudo tee -a /etc/ecs/ecs.config
-# sudo service ecs restart
+
 

9d8df0526804d055c17e1a0f829afe693256b91e
archaeogeek <jo.k.cook@gmail.com>
2021-02-25 12:56:39 +0000

    backing out of all of today's changes

--- docker/server_prep-os.sh
+++ docker/server_prep-os.sh
@@ -6,12 +6,6 @@ git clone https://github.com/AstunTechnology/os-custom-geonetwork.git
 chmod +x os-custom-geonetwork/docker/customisations-os.sh
 ./os-custom-geonetwork/docker/customisations-os.sh
 
-# add to ecs-config
-# sudo echo "ECS_ENABLE_AWSLOGS_EXECUTIONROLE_OVERRIDE=true" | sudo tee -a /etc/ecs/ecs.config
-# sudo echo "ECS_CLUSTER=docker-geonetwork-ec2" | sudo tee -a /etc/ecs/ecs.config
-# sudo service ecs restart
-
-
 
 # docker-bench-security issues 1.5-1.13
 # echo "-w /usr/bin/docker -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
@@ -26,7 +20,7 @@ chmod +x os-custom-geonetwork/docker/customisations-os.sh
 # sudo service auditd restart
 
 # docker-bench-security issues 2.1-2.15
-sudo mv ./os-custom-geonetwork/docker/docker-security/daemon.json /etc/docker/daemon.json
-sudo chown root:root /etc/docker/daemon.json
-sudo service docker restart
+# sudo mv ./os-custom-geonetwork/docker/docker-security/daemon.json /etc/docker/daemon.json
+# sudo chown root:root /etc/docker/daemon.json
+# sudo service docker restart
 

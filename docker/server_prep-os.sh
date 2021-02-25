01b8153b2a57a3beb757657f66d4a946562318eb
archaeogeek <jo.k.cook@gmail.com>
2021-02-25 11:38:15 +0000

    commenting out the audit stuff as well since we're not having success with any of the changes

--- docker/server_prep-os.sh
+++ docker/server_prep-os.sh
@@ -7,16 +7,16 @@ chmod +x os-custom-geonetwork/docker/customisations-os.sh
 ./os-custom-geonetwork/docker/customisations-os.sh
 
 # docker-bench-security issues 1.5-1.13
-echo "-w /usr/bin/docker -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
-echo "-w /var/lib/docker -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
-echo "-w /etc/docker -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
-echo "-w /lib/systemd/system/docker.service -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
-echo "-w /lib/systemd/system/docker.socket -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
-echo "-w /etc/default/docker -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
-echo "-w /etc/docker/daemon.json -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
-echo "-w /usr/bin/docker-containerd -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
-echo "-w /usr/bin/docker-runc -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
-sudo service auditd restart
+# echo "-w /usr/bin/docker -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
+# echo "-w /var/lib/docker -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
+# echo "-w /etc/docker -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
+# echo "-w /lib/systemd/system/docker.service -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
+# echo "-w /lib/systemd/system/docker.socket -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
+# echo "-w /etc/default/docker -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
+# echo "-w /etc/docker/daemon.json -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
+# echo "-w /usr/bin/docker-containerd -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
+# echo "-w /usr/bin/docker-runc -p wa" | sudo tee -a /etc/audit/rules.d/audit.rules
+# sudo service auditd restart
 
 # docker-bench-security issues 2.1-2.15
 #sudo mv ./os-custom-geonetwork/docker/docker-security/daemon.json /etc/docker/daemon.json

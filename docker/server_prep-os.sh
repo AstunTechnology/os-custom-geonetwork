fefd9fe48bdee5b18148b534ae247a2ff011f49b
archaeogeek <jo.k.cook@gmail.com>
2021-02-25 10:09:12 +0000

    Changed sitemap URL in robots.txt to match OS, added a custom docker daemon.json with docker-bench-security changes

--- docker/server_prep-os.sh
+++ docker/server_prep-os.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 yum -y update
 yum -y install unzip git
-cd /home/ec2-user && \
-git clone https://github.com/AstunTechnology/os-custom-geonetwork.git && \
-chmod +x os-custom-geonetwork/docker/customisations-os.sh && \
+cd /home/ec2-user
+git clone https://github.com/AstunTechnology/os-custom-geonetwork.git
+chmod +x os-custom-geonetwork/docker/customisations-os.sh
 ./os-custom-geonetwork/docker/customisations-os.sh
+sudo mv ./os-custom-geonetwork/docker/docker-security/daemon.json /etc/docker/daemon.json
+sudo chown root:root /etc/docker/daemon.json
+sudo service docker restart
+

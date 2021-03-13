f1bffd67696bb61ffe6c61dec674c3e6f76570e5
archaeogeek <jo.k.cook@gmail.com>
2021-03-13 18:11:16 +0000

    fixed customisations.sh

--- customisations.sh
+++ customisations.sh
@@ -4,10 +4,10 @@ cd /home/ec2-user && \
 curl -fSL https://github.com/AstunTechnology/iso19139.gemini23/archive/3.10.x.zip -o gemini23.zip && \
 mkdir -p schemas && \
 unzip -o gemini23.zip -d schemas && \
-curl -fSL https://github.com/AstunTechnology/iso19139.nonspatial/archive/master.zip -o nonspatial.zip && \
+curl -fSL https://github.com/AstunTechnology/iso19139.nonspatial/archive/main.zip -o nonspatial.zip && \
 unzip -o nonspatial.zip -d schemas && \
-curl -fSL https://github.com/metadata101/dcat2/archive/master.zip -o dcat.zip && \
-unzip -o dcat.zip -d schemas && \
+curl -fSL https://github.com/AstunTechnology/dcat2/archive/master.zip -o dcat.zip && \
+unzip -o dcat.zip -d schemas
 # directories
 mkdir -p pgdata esdata nginx kibana elasticsearch && \
 cp -rf /home/ec2-user/docker-geonetwork/nginx/* /home/ec2-user/nginx && \
--- schema_prep.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-yum -y install unzip
-cd /home/ec2-user && \
-curl -fSL https://github.com/AstunTechnology/iso19139.gemini23/archive/3.10.x.zip -o gemini.zip && \
-mkdir schemas && \
-unzip gemini.zip -d schemas && \
-echo -e "\nECS_ENGINE_AUTH_TYPE=dockercfg" >> /etc/ecs/ecs.config

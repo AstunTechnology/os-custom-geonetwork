0628d169ca7ce26d53221c64ec8998b936ad61b4
archaeogeek <jo.k.cook@gmail.com>
2021-04-16 12:28:17 +0100

    tweaks to docker-compose to include modified web.xml

--- elasticsearch/setup.sh
+++ elasticsearch/setup.sh
@@ -4,17 +4,17 @@ echo Initiating Elasticsearch Custom Index
 # move to the directory of this setup script
 cd "$(dirname "$0")"
 
-until $(curl -sSf -XGET  'http://localhost:9200/_cluster/health?wait_for_status=yellow' > /dev/null); do
+until $(curl -sSf -XGET  'http://localhost:9200/_cluster/health?wait_for_status=yellow' -u elastic:elastic > /dev/null); do
     printf 'not ready, trying again in 5 seconds \n'
     sleep 5
 done
 
 echo "Loading Indices into ElasticSearch"
-curl -s -XPUT -H 'Content-Type: application/json' http://0.0.0.0:9200/gn-records -d @/records.json
-curl -s -XPUT -H 'Content-Type: application/json' http://0.0.0.0:9200/gn-records-public -d @/records.json
-curl -s -XPUT -H 'Content-Type: application/json' http://0.0.0.0:9200/gn-features -d @/features.json
-curl -s -XPUT -H 'Content-Type: application/json' http://0.0.0.0:9200/gn-searchlogs -d @/searchlogs.json
+curl -s -XPUT -H 'Content-Type: application/json' http://0.0.0.0:9200/gn-records -u elastic:elastic -d @/records.json
+curl -s -XPUT -H 'Content-Type: application/json' http://0.0.0.0:9200/gn-records-public -u elastic:elastic -d @/records.json
+curl -s -XPUT -H 'Content-Type: application/json' http://0.0.0.0:9200/gn-features -u elastic:elastic -d @/features.json
+curl -s -XPUT -H 'Content-Type: application/json' http://0.0.0.0:9200/gn-searchlogs -u elastic:elastic -d @/searchlogs.json
 
 # else
 # 	echo "Indices already exist"
-#fi
+# fi

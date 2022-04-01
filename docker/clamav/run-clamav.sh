c65f7c49806d0ae8d999a8c8fbe36cc796b2e406
archaeogeek <jo.k.cook@gmail.com>
2022-04-01 12:39:38 +0100

    fixed antivirus script

--- docker/clamav/run-clamav.sh
+++ docker/clamav/run-clamav.sh
@@ -3,6 +3,10 @@
 # pull and run clamav image, scanning docker volumes
 # send output to clamav-logs/output.txt
 # move infected files to clamav-quarantine
+
+#unset $DOCKER_CONTENT_TRUST because the av container is not signed
+unset DOCKER_CONTENT_TRUST
+
 docker run --rm -it -v /var/lib/docker/volumes:/scan -v /home/ec2-user/clamav-logs:/logs -v /home/ec2-user/clamav-quarantine:/quarantine tquinnelly/clamav-alpine  --log=logs/output.txt --move=quarantine
 
 #  read the SMTP environment variables from the .env file
@@ -11,11 +15,16 @@ set -a; source /home/ec2-user/.env; set +a
 # add a line to output.txt so we know the cron job has run even if
 # clamav doesn't, because of network error or whatever
 
-sudo echo "Antivirus job ran, but if this is the only line then no output was generated\n"  | tee -a /home/ec2-user/clamav-logs/output.txt
+if [ ! -f /home/ec2-user/clamav-logs/output.txt ]; then
+        sudo echo -e "Antivirus job ran, but no output was generated\n" >> /home/ec2-user/clamav-logs/output.txt
+fi
 
 # send output file as email using curl
-sudo curl --ssl-reqd   --url smtps://$SMTP   --user $SMTPUSER:$SMTPPWD   --mail-from $EMAILADDR   --mail-rcpt $EMAILADDR   --upload-file /home/ec2-user/clamav-logs/output.txt
+sudo curl -v --url smtps://$SMTP --ssl-reqd  --mail-from $EMAILADDR --mail-rcpt $EMAILADDR  --user $SMTPUSER:$SMTPPWD -F '=</home/ec2-user/clamav-logs/output.txt;encoder=quoted-printable' -H "Subject: $ES_PREFIX  antivirus output $(date +%Y-%m-%d)" -H "From: $EMAILADDR <$EMAILADDR>" -H "To: $EMAILADDR <$EMAILADDR>"
+
 
 # remove the log file
 sudo rm /home/ec2-user/clamav-logs/output.txt
 
+# reset $DOCKER_CONTENT_TRUST
+export DOCKER_CONTENT_TRUST=1
